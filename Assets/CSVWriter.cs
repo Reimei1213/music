3f28100ae8fe702cd1b0f53139b2c94c6dbd4362
Reimei1213 <kakibo1213@gmail.com>
2017-12-16 19:52:54 +0900

    音ゲーのはじめを作りました

---
+++ Assets/CSVWriter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;  // <- ここに注意
+
+
+public class CSVWriter : MonoBehaviour {
+
+	public string fileName; // 保存するファイル名
+
+
+	 void Start () {
+	     WriteCSV ("Hello,World");
+	 }
+
+	// CSVに書き込む処理
+	public void WriteCSV(string txt){
+		StreamWriter streamWriter;
+		FileInfo fileInfo;
+		fileInfo = new FileInfo (Application.dataPath +"/"+ fileName + ".csv");
+		streamWriter = fileInfo.AppendText ();
+		streamWriter.WriteLine (txt);
+		streamWriter.Flush();
+		streamWriter.Close ();
+	}
+}
